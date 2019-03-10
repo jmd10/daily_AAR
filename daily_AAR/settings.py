4abdd44f89d45afb609882cccab5b2454a3ac505
jmd10 <jason@lvl6.xyz>
2019-03-09 19:24:01 -0500

    Fix toggle collapse

--- daily_AAR/settings.py
+++ daily_AAR/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r8p=t62cllxmn$t@0%h_vi9wg5ef8lwh-na&zl2klatqq3_+^#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['localhost']
 
 
 
